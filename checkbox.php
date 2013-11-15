09a65b8a662fdaf5f778cffdacfc7db05a307bdb
nicolasruslim <nicolasruslim1@gmail.com>
2013-11-15 09:17:11 +0700

    Update checkbox.php

--- checkbox.php
+++ checkbox.php
@@ -1,3 +1,8 @@
+//Dibuat oleh:
+//Gunawan 18211007
+//Nurafni Eka Agustina 18211012
+//Nicolas Novian Ruslim/18211031
+
 <form action="hitungtotal.php" method="post">		
 				<input type="checkbox" name="check_list[]" value= "Nasi Putih">Nasi Putih<BR>			
 				<input type="checkbox" name="check_list[]" value= "Ayam Goreng">Ayam Goreng<BR>
